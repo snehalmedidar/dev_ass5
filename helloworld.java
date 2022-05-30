6b0b08d0033036ae0ae65d4c66f675a1132d0399
snehalmedidar <87894135+snehalmedidar@users.noreply.github.com>
2022-05-30 12:27:49 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,5 @@
-
-public class HelloWorld
-{
-	// Your program begins with a call to main().
-	// Prints "Hello, World" to the terminal window.
-	public static void main(String args[])
-	{
-		System.out.println("Hello, World");
-	}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world..!!!");
+    }
 }
